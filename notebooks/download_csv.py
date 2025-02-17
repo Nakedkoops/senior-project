41f1c4c7d004984c45adf4c53b4d9bb742b6af35
Nakedkoops <115908645+Nakedkoops@users.noreply.github.com>
2025-02-17 14:05:24 -0500

    creating base python code

---
+++ notebooks/download_csv.py
@@ -0,0 +1,42 @@
+from astropy.io import fits
+from astropy.table import Table, vstack
+from multiprocessing.pool import ThreadPool
+import requests
+import matplotlib.pyplot as plt
+import numpy as np
+import cudf
+import csv
+import io
+
+# For this section, read the skysever-dump.csv file and condense it into
+# a seperate csv file for later use
+
+def process_transmit_files_table(url: str):
+    try:
+        file = io.BytesIO(requests.get(url).content)
+
+        with fits.open(file) as hdul:
+            table = Table.read(hdul["COADD"])
+            table.keep_columns(["flux", "model"])
+
+        return table
+    
+    except Exception as e:
+        print(f"Failed to process {url}: {e}")
+        return None
+    
+
+with open("./skyserver-dump.csv", newline="") as file:
+    reader = csv.DictReader(file)
+    urls = [row["url"] for row in reader]
+    hdul = fits.open(urls[1])
+    
+fits_tables = ThreadPool(8).imap_unordered(process_transmit_files_table, urls)
+
+print(hdul["PRIMARY"].header)
+
+fits_tables = [t for t in fits_tables if t is not None]
+
+final_table = vstack(fits_tables)
+
+final_table.write("final_output.fits", format = "fits", overwrite = True)
